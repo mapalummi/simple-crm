d653c78825ed6cfb08ce043b0f1e09df7836659f
Marco <marco.palummieri@gmail.com>
2025-08-23 22:51:40 +0200

    Refactor user component to use `inject` for MatDialog service; update imports and add standalone flag.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
+import { Component, inject } from '@angular/core'; // `inject` hier importieren
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
+  standalone: true, // <--- Das ist wichtig!
   imports: [MatIconModule, MatTooltipModule, MatButtonModule, MatDialogModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
+  // Mit `inject` bekommst du eine Instanz des `MatDialog` Services direkt in einer Eigenschaft. Kein Konstruktor mehr nötig, wenn dieser keine andere Aufgabe hat.
+  readonly dialog = inject(MatDialog);
 
-openDialog(){
-
-}
-
+  openDialog() {
+    this.dialog.open(DialogAddUserComponent)
+  }
 }
