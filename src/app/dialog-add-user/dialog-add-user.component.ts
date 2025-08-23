e9dad6f2e389926f3d4677858dba734c039fabcf
Marco <marco.palummieri@gmail.com>
2025-08-23 22:13:55 +0200

    Add user dialog component with template and styles; implement button to open dialog

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  imports: [],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss'
+})
+export class DialogAddUserComponent {
+
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user',
-  imports: [MatIconModule, MatDividerModule, MatButtonModule],
+  imports: [MatIconModule, MatTooltipModule, MatButtonModule, MatDialogModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+openDialog(){
+
+}
 
 }
