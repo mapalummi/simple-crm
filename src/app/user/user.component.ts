d4c18cd9a3fe48da5a6dbc569e8523bebfcbc8cc
Marco <marco.palummieri@gmail.com>
2025-08-23 21:19:48 +0200

    Add button with icon to user component; include necessary Angular Material modules

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
