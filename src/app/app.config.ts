0e34519406cf5d8d26c2abb55ba839d2524e63cd
Marco <marco.palummieri@gmail.com>
2025-08-25 16:56:03 +0200

    feat: integrate Firebase into Angular application
    - Added @angular/fire and firebase dependencies in package.json.
    - Configured Firebase initialization in app.config.ts with project details.
    - Updated dialog-add-user component to change datepicker icon for better UX.
    - Created firebase.json for Firebase configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "simple-crm-3fe9a", appId: "1:345942422370:web:215a5ea70fe4a53e8e723b", storageBucket: "simple-crm-3fe9a.firebasestorage.app", apiKey: "AIzaSyCbWv-yF3G3TRpPqS8289ZSkFiaRVTm7ms", authDomain: "simple-crm-3fe9a.firebaseapp.com", messagingSenderId: "345942422370" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ],
 };
