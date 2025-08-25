e64e25fc33b1291fcb17780593b0fc7eca2fcd2c
Marco <marco.palummieri@gmail.com>
2025-08-25 10:14:47 +0200

    Bind user input fields to the user model in the dialog-add-user component; implement saveUser method to log user data.

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <--- Das ist wichtig für ngModel!
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
+import { User } from '../../models/user.class'; // Verknüpfung zur user.class.ts !!!
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
   providers: [provideNativeDateAdapter()],
   imports: [
+    FormsModule, // <--- Das ist wichtig für ngModel!
     MatDialogModule,
     MatButtonModule,
     MatInputModule,
@@ -23,4 +26,13 @@ import {MatIconModule} from '@angular/material/icon';
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
-export class DialogAddUserComponent {}
+export class DialogAddUserComponent {
+  // Variable und neue Instanz für neuen User erstellen:
+  user = new User();
+
+  constructor() {}
+
+  saveUser() {
+    console.log('Current user is', this.user);
+  }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,6 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
+import { User } from '../../models/user.class'; // Verknüpfung zur user.class.ts !!!
+
 @Component({
   selector: 'app-user',
   standalone: true, // <--- Das ist wichtig!
@@ -15,6 +17,11 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 export class UserComponent {
   // Mit `inject` bekommst du eine Instanz des `MatDialog` Services direkt in einer Eigenschaft. Kein Konstruktor mehr nötig, wenn dieser keine andere Aufgabe hat.
   readonly dialog = inject(MatDialog);
+
+  // Variable und neue Instanz für neuen User erstellen:
+  user = new User();
+
+  constructor() {}
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent);
