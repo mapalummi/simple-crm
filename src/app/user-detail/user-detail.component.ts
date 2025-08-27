7bd96c1aaa1fc9149c8fe3c11ea3f91d5ae9730e
Marco <marco.palummieri@gmail.com>
2025-08-27 16:48:50 +0200

    Implement code changes to enhance functionality and improve performance

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
