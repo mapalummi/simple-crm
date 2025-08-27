7df105681eb898ab962cae35beea848d49796ac9
Marco <marco.palummieri@gmail.com>
2025-08-27 21:51:11 +0200

    feat: implement user detail retrieval and display with dynamic data binding

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core'; // OnInit neu
+import { Component, inject, OnInit } from '@angular/core'; // OnInit neu
 import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router'; // NEU
+import { User } from '../../models/user.class';
+import { Firestore, collection, doc, getDoc } from '@angular/fire/firestore';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 @Component({
   selector: 'app-user-detail',
-  imports: [MatCardModule],
+  standalone: true,
+  imports: [MatCardModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss',
 })
 export class UserDetailComponent implements OnInit {
   // implements OnInit neu
 
+  firestore: Firestore = inject(Firestore);
+
   userId = ''; // Variable gesetzt
+  user: User | null = null; // NEU
 
   constructor(private route: ActivatedRoute) {} // NEU
 
@@ -19,6 +28,26 @@ export class UserDetailComponent implements OnInit {
     this.route.paramMap.subscribe((paramMap) => {
       this.userId = paramMap.get('id') || ''; // So ist userId immer ein String !!! 'id' weil in den Routes so festgelegt.
       console.log('GOT ID', this.userId);
+      this.getUser();
     });
+  }
+
+  // TODO: Mit Firebase Doku abgleichen !!!
+  async getUser() {
+    if (!this.userId) return;
+    const userDocRef = doc(this.firestore, 'users', this.userId);
+    const userSnap = await getDoc(userDocRef);
+    if (userSnap.exists()) {
+      const data = userSnap.data();
+      this.user = new User({ ...data, id: userSnap.id }); // <-- Typisierung und ID
+      console.log('Retrieved user', this.user);
+    } else {
+      this.user = null;
+      console.log('Found no user!');
+    }
+  }
+
+  openAddressDialog(){
+
   }
 }
