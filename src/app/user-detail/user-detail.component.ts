ad4ebd67899b34632f0867a7c2ad11351f64792c
Marco <marco.palummieri@gmail.com>
2025-08-27 17:26:29 +0200

    feat: enhance user detail component with routing and styling improvements

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; // OnInit neu
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router'; // NEU
 
 @Component({
   selector: 'app-user-detail',
   imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrl: './user-detail.component.scss',
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+  // implements OnInit neu
 
+  userId = ''; // Variable gesetzt
+
+  constructor(private route: ActivatedRoute) {} // NEU
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap) => {
+      this.userId = paramMap.get('id') || ''; // So ist userId immer ein String !!! 'id' weil in den Routes so festgelegt.
+      console.log('GOT ID', this.userId);
+    });
+  }
 }
