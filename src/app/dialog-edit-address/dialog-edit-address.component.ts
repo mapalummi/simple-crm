0a371fa47de95c13b0a39bc081f238569d2a078c
Marco <marco.palummieri@gmail.com>
2025-08-28 10:35:48 +0200

    feat: update dialog-edit-address component to improve form field structure and include MatInputModule

--- src/app/dialog-edit-address/dialog-edit-address.component.ts
+++ src/app/dialog-edit-address/dialog-edit-address.component.ts
@@ -5,10 +5,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
 import { User } from '../../models/user.class';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dialog-edit-address',
-  imports: [MatDialogModule, MatFormFieldModule, FormsModule],
+  imports: [MatDialogModule, MatFormFieldModule, FormsModule, MatInputModule],
   templateUrl: './dialog-edit-address.component.html',
   styleUrl: './dialog-edit-address.component.scss',
 })
